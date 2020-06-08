f2d733ae1024250423c88375c8a5b2b03ef1b47f
Tomohiro Uno <uuuteenet@gmail.com>
2020-06-08 09:17:45 +0900

    Replace temp with query
    base_price をメソッドに置き換えたことで、抽出が可能になったので、 discount_factor もメソッドに置き換える

--- chapter6/6-4_replace_temp_with_query.rb
+++ chapter6/6-4_replace_temp_with_query.rb
@@ -1,12 +1,11 @@
 def price
-  if (base_price > 1000)
-    discount_factor = 0.95
-  else
-    discount_factor = 0.98
-  end
   base_price * discount_factor
 end
 
 def base_price
   @quantity * @item_price
+end
+
+def discount_factor
+  base_price > 1000 ? 0.95 : 0.98
 end

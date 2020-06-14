347b737aff0421394e28b698b3bcb022cc808082
Tomohiro Uno <uuuteenet@gmail.com>
2020-06-14 17:29:28 +0900

    Remove unnecessary comment
    説明用変数に切り出したことで、不要になったコメントを削除する

--- chapter6/6-6_introduce_explaining_variable.rb
+++ chapter6/6-6_introduce_explaining_variable.rb
@@ -1,5 +1,4 @@
 def price
-  # 価格は、基本価格 - 数量割引 + 配送料
   base_price = @quantity * @item_price
   quantity_discount = [0, @quantity - 500].max * @item_price * 0.05
   shipping = [base_price * 0.1, 100.0].min

a549436d8aefb10c58f4a76d28afdfa865585294
Tomohiro Uno <uuuteenet@gmail.com>
2020-07-20 19:59:21 +0900

    Implements initializer
    イニシャライザを実装して、元のメソッドから移行できるようにする

--- chapter6/6-9_replace_method_with_method_object.rb
+++ chapter6/6-9_replace_method_with_method_object.rb
@@ -19,4 +19,11 @@ class Gamma
               :important_value1,
               :important_value2,
               :important_value3
+
+  def initialize(account, input_val_arg, quantity_arg, year_to_date_arg)
+    @account = account
+    @input_val = input_val_arg
+    @quantity = quantity_arg
+    @year_to_date = year_to_date_arg
+  end
 end

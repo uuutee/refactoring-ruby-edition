60a72a87cb7cd1b06b0de668d6ed72aa223c7448
Tomohiro Uno <uuuteenet@gmail.com>
2020-09-23 21:05:18 +0900

    Introduce named parameters

--- chapter6/6-15_introduce_named_parameter_2.rb
+++ chapter6/6-15_introduce_named_parameter_2.rb
@@ -1,12 +1,15 @@
 class Books
-  def self.find(selector, conditions="", *joins)
+  def self.find(selector, hash={})
+    hash[:joins] ||= []
+    hash[:conditions] ||= ""
+
     sql = ["SELECT * FROM books"]
-    joins.each do |join_table|
+    hash[:joins].each do |join_table|
       sql << "LEFT OUTER JOIN #{join_table} ON"
-      sql << "books.#{join_table.to_s.chap}_id"
+      sql << "books.#{join_table.to_s.chop}_id"
       sql << " = #{join_table}.id"
     end
-    sql << "WHERE #{conditions}" unless conditions.empty?
+    sql << "WHERE #{hash[:conditions]}" unless hash[:conditions].empty?
     sql << "LIMIT 1" if selector == :first
 
     connection.find(sql.join(" "))
@@ -14,7 +17,6 @@ class Books
 end
 
 Books.find(:all)
-Books.find(:all, "title like '%Voodoo Economics'")
-Books.find(:all, "authors.name = 'Jenny James'", :authors)
-Books.find(:first, "authors.name = 'Jenny James'", :authors)
-
+Books.find(:all, :conditions => "title like '%Voodoo Economics'")
+Books.find(:all, :conditions => "authors.name = 'Jenny James'", :joins => [:authors])
+Books.find(:first, :conditions => "authors.name = 'Jenny James'", :joins => [:authors])

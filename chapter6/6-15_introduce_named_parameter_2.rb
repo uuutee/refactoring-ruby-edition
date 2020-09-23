9a5f8cf731435e0742d174bf8f248f24a3cbe5e0
Tomohiro Uno <uuuteenet@gmail.com>
2020-09-23 20:59:51 +0900

    Add example

---
+++ chapter6/6-15_introduce_named_parameter_2.rb
@@ -0,0 +1,20 @@
+class Books
+  def self.find(selector, conditions="", *joins)
+    sql = ["SELECT * FROM books"]
+    joins.each do |join_table|
+      sql << "LEFT OUTER JOIN #{join_table} ON"
+      sql << "books.#{join_table.to_s.chap}_id"
+      sql << " = #{join_table}.id"
+    end
+    sql << "WHERE #{conditions}" unless conditions.empty?
+    sql << "LIMIT 1" if selector == :first
+
+    connection.find(sql.join(" "))
+  end
+end
+
+Books.find(:all)
+Books.find(:all, "title like '%Voodoo Economics'")
+Books.find(:all, "authors.name = 'Jenny James'", :authors)
+Books.find(:first, "authors.name = 'Jenny James'", :authors)
+

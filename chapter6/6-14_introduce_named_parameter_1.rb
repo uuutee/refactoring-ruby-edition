75e5ea3bb9b6773195eb9a89a24208a7a0413f29
Tomohiro Uno <uuuteenet@gmail.com>
2020-09-23 20:00:51 +0900

    Add example

---
+++ chapter6/6-14_introduce_named_parameter_1.rb
@@ -0,0 +1,11 @@
+class SearchCriteria
+  attr_reader :author_id, :publisher_id, :isbn
+
+  def initialize(author_id, publisher_id, isbn)
+    @author_id = author_id
+    @publisher_id = publisher_id
+    @isbn = isbn
+  end
+end
+
+criteria = SearchCriteria.new(5, 8, "0201485672")

41021d75da9334e609de6bb71e621e51a469426f
Tomohiro Uno <uuuteenet@gmail.com>
2020-10-14 23:35:41 +0900

    Extend dynamic method definition module
    動的に定義されたメソッドを持つModuleをextendしてメソッドを定義する

--- chapter6/6-17_dynamic_method_definition_3.rb
+++ chapter6/6-17_dynamic_method_definition_3.rb
@@ -1,11 +1,22 @@
-class PostData
-  def initialize(post_data)
-    (class << self; self; end).class_eval do
-      post_data.each_pair do |key, value|
-        define_method key.to_sym do
+class Hash
+  def to_module
+    hash = self
+    Module.new do
+      hash.each_pair do |key, value|
+        define_method key do
           value
         end
       end
     end
+  end
+end
+
+class PostData
+  def initialize(post_data)
+    # 1. 受け取ったHashの #to_module を呼び出す
+    # 2. #to_module は Hash のキー名を動的にメソッド化した Module を返す
+    # 3. 動的に定義されたメソッドの Module を extend (継承) する
+    # 4. インスタンス内で動的なメソッドが利用できるようになる
+    self.extend post_data.to_module
   end
 end
